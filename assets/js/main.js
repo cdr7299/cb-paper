002ed3bfee49ce6a24d09b399e924c9b71671959
Vineet <Vineet@gmail.com>
2017-07-08 15:26:36 +0530

    just save it

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+// window.onload = function(){
+// 	// for (var i = 0; i<25;i++){
+// 		$('#table-box1')
+// 		    .on("mouseenter", function () {
+// 	    	    $("#table-box-hidden").show();            
+// 	    	})
+// 	   		.on("mouseleave", function () {
+// 	        	$("#table-box-hidden").hide();
+// 	    	});
+//   // }
+// }
+
+for(i=1;i<=21;i++){
+
+$('#table-box'+ i)
+    .mouseover(function(){
+
+    	$()
+
+
+    })
